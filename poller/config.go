68ec4a74a061334fcc07d3cf0d66e054fb903a12
Ellen Choi <ellen@polyweave.xyz>
2023-05-26 16:07:29 -0400

    adding config to poller to enable trace-only polling

--- poller/config.go
+++ poller/config.go
@@ -7,12 +7,13 @@ import (
 )
 
 type Config struct {
-	Blockchain  constants.Blockchain `env:"BLOCKCHAIN,required"`
-	BatchSize   int                  `env:"BATCH_SIZE" envDefault:"100"`
-	ReorgDepth  int                  `env:"REORG_DEPTH" envDefault:"8"`
-	HttpRetries int                  `env:"HTTP_RETRIES" envDefault:"10"`
-	SleepTime   time.Duration        `env:"POLLER_SLEEP_TIME" envDefault:"12s"`
-	Tick        time.Duration        `env:"POLLER_TICK_DURATION" envDefault:"1s"`
-	AutoStart   bool                 `env:"POLLER_AUTO_START" envDefault:"false"`
-	CursorKey   string               `env:"CURSOR_KEY" envDefault:""`
+	Blockchain      constants.Blockchain `env:"BLOCKCHAIN,required"`
+	BatchSize       int                  `env:"BATCH_SIZE" envDefault:"100"`
+	ReorgDepth      int                  `env:"REORG_DEPTH" envDefault:"8"`
+	HttpRetries     int                  `env:"HTTP_RETRIES" envDefault:"10"`
+	SleepTime       time.Duration        `env:"POLLER_SLEEP_TIME" envDefault:"12s"`
+	Tick            time.Duration        `env:"POLLER_TICK_DURATION" envDefault:"1s"`
+	AutoStart       bool                 `env:"POLLER_AUTO_START" envDefault:"false"`
+	CursorKey       string               `env:"CURSOR_KEY" envDefault:""`
+	IsTraceBackfill bool                 `env:"IS_TRACE_BACKFILL" envDefault:"false"`
 }
